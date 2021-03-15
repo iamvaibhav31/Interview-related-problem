de8d2615a43bd3efe1c1149680a6a73d647960e9
Thegoodspirit <80534369+Thegoodspirit@users.noreply.github.com>
2021-03-15 22:51:14 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,3 +1,10 @@
+"""
+A good example problem for showing algorithms with different orders of magnitude is the classic anagram detection problem for strings. One string is an anagram of another 
+if the second is simply a rearrangement of the first. For example, 'heart' and 'earth' are anagrams. The strings 'python' and 'typhon' are anagrams as well. For the sake of 
+simplicity, we will assume that the two strings in question are of equal length and that they are made up of symbols from the set of 26 lowercase alphabetic characters. 
+Our goal is to write a boolean function that will take two strings and return whether they are anagrams.
+"""
+
 def anagram(s1 , s2): # methord 1
     s1 = s1.replace(' ','').lower()
     s2 = s1.replace(' ','').lower()
