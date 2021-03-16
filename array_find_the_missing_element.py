cd0c26717459e0a363d20af3f5c39c08fd31a2c2
Thegoodspirit <80534369+Thegoodspirit@users.noreply.github.com>
2021-03-16 23:01:33 +0530

    Update array_find_the_missing_element.py

--- array_find_the_missing_element.py
+++ array_find_the_missing_element.py
@@ -52,4 +52,4 @@ if __name__="__main__":
     arr1 = [1,2,3,4,5,6,7]
     arr2 = [3,7,2,1,4,6] 
     print("The missing element is :- ",finder(arr1,arr2))
-print("The missing element is :- ",finder1(arr1,arr2))
+    print("The missing element is :- ",finder1(arr1,arr2))
