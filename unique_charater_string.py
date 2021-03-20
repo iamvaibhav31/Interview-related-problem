5c2c6386dbfd3dc95061ec87641aeb7673165c4e
vaibhav  sharma <80534369+Thegoodspirit@users.noreply.github.com>
2021-03-20 23:03:01 +0530

    Update unique_charater_string.py

--- unique_charater_string.py
+++ unique_charater_string.py
@@ -1,14 +1,16 @@
 """
-Given a string , determine if it is compreised of all the
-unique character .for example the string 'abcde' has all 
-unique characters and should return True. The string 
-'aabbss' contain dublicate character and shaould retune 
-false
+Given a string , determine if it is compreised of all the unique character .for example the string 'abcde' has all unique characters and should return True. The string 'aabbss' 
+contain dublicate character and shaould retune false
 """
-
+# ___ METHORD 1 ___ #  
+# In this methor we use python inbuild function to solve this problem 
 def uni_char(s):
     return len(set(s)) == len(s)
+# THIS METHORD TAKING LESS MEMORY ADDRESS OR LESS EXECUTION TIME 
+# ___ END ___ #
 
+# ____ METHORD 2 ____ #
+# In this methord we are using manul methord to solved this solution 
 def uni_char1(s):
 
     chars = set()
@@ -20,7 +22,11 @@ def uni_char1(s):
             chars.add(letter)
     
     return True
+# BUT THIS TAKE LARGE AMOUNT OF MEMORY SPACE OR EXECUTION TIME 
+# ___ END ___ # 
 
-print(uni_char("aarriivm"))
-print(uni_char1('abcde'))
+# ___ START ___ #
+if __name__ == "__main__":
+    print(uni_char("aarriivm"))
+    print(uni_char1('abcde'))
      
