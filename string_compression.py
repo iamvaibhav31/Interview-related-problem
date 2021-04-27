283748cad09f54357453d743ab1bdf913befcc47
vaibhav  sharma <80534369+Thegoodspirit@users.noreply.github.com>
2021-04-27 14:27:47 +0530

    Update string_compression.py

--- string_compression.py
+++ string_compression.py
@@ -12,13 +12,13 @@ def compress(s):
 
     r = ""
     l = len(s)
-
+     #BEST_CASE
     if l == 0:
         return ""
-    
+     #AVERAGE_CASE
     if l == 1:
         return s+"1"
-
+    #WORST_CASE
     last = s[-1]
     cnt = 1
     i = 1
@@ -40,12 +40,13 @@ def compress(s):
 # METHORD_2
 
 def compress1(word):
+    #BEST_CASE
     if len(word) == 0:
         return ""
-    
+    #AVERAGE_CASE
     if lle(word) == 1:
         return word+"1"
-    
+    #WORST_CASE
     layer = []
     for i in word:
         if i not in layer:
